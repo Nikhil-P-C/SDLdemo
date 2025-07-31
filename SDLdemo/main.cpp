5eddc28552fed23dc2cba80d8bb5e594561bb338
Nikhil-P-C <nc3030303040@gmail.com>
2025-07-31 13:39:00 +0530

    cleaning code

--- SDLdemo/main.cpp
+++ SDLdemo/main.cpp
@@ -17,32 +17,139 @@ struct player {
 	int x, y; // Position of the player
 	
 };
-
-void eventHandler(SDL_Event& event,bool& running,int* x,int* y,int* clicks) {
-	// Handle events here
-    switch (event.type) {
-    case SDL_QUIT: std::cerr << "switch quit\n";
-        running = false;
-        break;
-    case SDL_KEYDOWN:std::cerr << "key down" << SDL_GetKeyName(event.key.keysym.sym) << "\n";
-        break;
-    case  SDL_MOUSEBUTTONDOWN:
-        std::cerr << "Mouse button down event received at ("<< event.button.x << ", "<< event.button.y << ")\n";
-        if (event.button.y > 600 || event.button.x > 800)std::cerr << "garbage click\n";
-        if ((event.button.x > *x && event.button.x < *x + 200) && (event.button.y >= *y && event.button.y <= *y + 200)) {
-            std::cerr << "Mouse clicked on the rectangle at (" << event.button.x << ", " << event.button.y << ")\n";(*clicks)++;
-			std::cerr << "Click count: " << *clicks << "\n";
+void enetEventHandler(ENetEvent *enetEvent, ENetHost* Host) {
+	 while (enet_host_service(Host, enetEvent, 0) > 0) {
+        switch (enetEvent->type) {
+        case ENET_EVENT_TYPE_CONNECT:
+            std::cout << "A new client connected from " << enetEvent->peer->address.host << ":" << enetEvent->peer->address.port << "\n";
+            break;
+        case ENET_EVENT_TYPE_RECEIVE:
+            std::cout << "A packet of length " << enetEvent->packet->dataLength << " was received from a client.\n";
+            enet_packet_destroy(enetEvent->packet); // Destroy the packet after processing
+            break;
+        case ENET_EVENT_TYPE_DISCONNECT:
+            std::cout << "A client disconnected.\n";
+            break;
+        default:
+            break;
         }
-        else {
-            std::cerr << "Mouse clicked outside the rectangle at (" << event.button.x << ", " << event.button.y << ")\n";
-        }
-
-        break;
-    default:
-        // Handle other events if necessary
-        break;
     }
 }
+
+void eventHandler(SDL_Event& event, bool& running, int* x, int* y, int* clicks, GameStates* CurrentState, std::string* textInput, std::string* serverTextInput) {
+
+
+
+    while (SDL_PollEvent(&event)) {
+        // Handle events here
+        switch (event.type) {
+        case SDL_QUIT: std::cerr << "switch quit\n";
+            running = false;
+            break;
+        case SDL_KEYDOWN:std::cerr << "key down" << SDL_GetKeyName(event.key.keysym.sym) << "\n";
+            break;
+        case  SDL_MOUSEBUTTONDOWN:
+            std::cerr << "Mouse button down event received at (" << event.button.x << ", " << event.button.y << ")\n";
+            if (event.button.y > 600 || event.button.x > 800)std::cerr << "garbage click\n";
+            if ((event.button.x > *x && event.button.x < *x + 200) && (event.button.y >= *y && event.button.y <= *y + 200)) {
+                std::cerr << "Mouse clicked on the rectangle at (" << event.button.x << ", " << event.button.y << ")\n";(*clicks)++;
+                std::cerr << "Click count: " << *clicks << "\n";
+            }
+            else {
+                std::cerr << "Mouse clicked outside the rectangle at (" << event.button.x << ", " << event.button.y << ")\n";
+            }
+
+            break;
+        default:
+            // Handle other events if necessary
+            break;
+        }
+        if (event.type == SDL_KEYDOWN) {
+            if (event.key.keysym.sym == SDLK_RETURN && !(*CurrentState == serverTEXTINPUT)) {
+
+                if (*CurrentState == TEXTINPUT) {
+                    std::cout << "Text input: " << *textInput << "\n"; // Print the text input to console
+                    SDL_StopTextInput();// Stop text input when Escape is pressed
+                    *CurrentState = MOVEMENT; // Switch to movement state
+                    std::cout << *CurrentState << "\n"; // Print the current state to console    
+                }
+                else {
+                    std::cout << "gamemode/\n";
+                    SDL_StartTextInput(); // Start text input when Escape is pressed
+                    *CurrentState = TEXTINPUT; // Switch to text input state
+                    std::cout << *CurrentState << "\n"; // Print the current state to console
+
+                }
+            }
+            //chat packets 
+
+
+            else if (event.key.keysym.sym == SDLK_BACKSPACE && *CurrentState == serverTEXTINPUT) {
+                // Handle backspace to remove the last character
+                if (!(serverTextInput->empty()))serverTextInput->pop_back();
+
+            }
+            else if (*CurrentState == serverTEXTINPUT && (event.key.keysym.sym == SDLK_ESCAPE || event.key.keysym.sym == SDLK_RETURN)) {
+                // Stop server text input when Escape is pressed
+                *CurrentState = MOVEMENT; // Switch back to movement state
+                SDL_StopTextInput(); // Stop text input
+                std::cout << "server text input stopped\n";
+                std::cout << "server text input: " << *serverTextInput << "\n"; // Print the server text input to console
+
+            }
+            else if (event.key.keysym.sym == SDLK_t && !(*CurrentState == TEXTINPUT)) {
+                if (*CurrentState == TEXTINPUT || *CurrentState == MOVEMENT) {
+                    SDL_StartTextInput(); // Start text input when 't' is pressed
+                    *CurrentState = serverTEXTINPUT; // Switch to server text input state
+                    *serverTextInput = ""; // Clear previous server text input
+                    std::cout << "server text input\n";
+                }
+
+            }
+            else if (event.key.keysym.sym == SDLK_ESCAPE) {
+                // Stop text input when Escape is pressed
+                SDL_StopTextInput();
+                *CurrentState = MOVEMENT; // Switch to movement state
+                std::cout << "text input stopped\n";
+            }
+            else if (event.key.keysym.sym == SDLK_BACKSPACE) {
+                // Handle backspace to remove the last character
+                if (!(textInput->empty()))textInput->pop_back();
+
+            }
+        }
+        else if (event.type == SDL_TEXTINPUT && *CurrentState == serverTEXTINPUT) {
+            // Handle text input here if needed
+            *serverTextInput += *event.text.text; // Append the new text input
+
+        }
+        else if (event.type == SDL_TEXTINPUT && *CurrentState == TEXTINPUT) {
+            // Handle text input here if needed
+            *textInput += *event.text.text; // Append the new text input
+        }
+    }
+    if (*CurrentState == MOVEMENT) {
+        const Uint8* keystate = SDL_GetKeyboardState(NULL);
+        if (*x <= 0) *x = 0;
+        if (*x >= 600) *x = 600;  // 800 - rect width
+        if (*y <= 0) *y = 0;
+        if (*y >= 400) *y = 400;  // 600 - rect height
+
+        if (keystate[SDL_SCANCODE_W]) {
+            *y -= 5;
+        }
+        if (keystate[SDL_SCANCODE_S]) {
+            *y += 5;
+        }
+        if (keystate[SDL_SCANCODE_A]) {
+            *x -= 5;
+        }
+        if (keystate[SDL_SCANCODE_D]) {
+            *x += 5;
+        }
+    }
+}
+
 
 int main(int argc, char* argv[]) {
     // Initialize SDL
@@ -157,119 +264,12 @@ int main(int argc, char* argv[]) {
     ENetEvent enetEvent;
 
     while (running) {
-        while (enet_host_service(Host, &enetEvent, 0) > 0) {
-            switch (enetEvent.type) {
-            case ENET_EVENT_TYPE_CONNECT:
-                std::cout << "A new client connected from " << enetEvent.peer->address.host << ":" << enetEvent.peer->address.port << "\n";
-                break;
-            case ENET_EVENT_TYPE_RECEIVE:
-                std::cout << "A packet of length " << enetEvent.packet->dataLength << " was received from a client.\n";
-                enet_packet_destroy(enetEvent.packet); // Destroy the packet after processing
-                break;
-            case ENET_EVENT_TYPE_DISCONNECT:
-                std::cout << "A client disconnected.\n";
-                break;
-            default:
-                break;
-            }
-        }
-
-        while (SDL_PollEvent(&inputEvent)) {
-            eventHandler(inputEvent, running, &x, &y, &clicks);
-            if (inputEvent.type == SDL_KEYDOWN) {
-                if (inputEvent.key.keysym.sym == SDLK_RETURN && !(CurrentState == serverTEXTINPUT)) {
-
-                    if (CurrentState == TEXTINPUT) {
-                        std::cout << "Text input: " << textInput << "\n"; // Print the text input to console
-                        SDL_StopTextInput();// Stop text input when Escape is pressed
-                        CurrentState = MOVEMENT; // Switch to movement state
-                        std::cout << CurrentState << "\n"; // Print the current state to console    
-                    }
-                    else {
-                        std::cout << "gamemode/\n";
-                        SDL_StartTextInput(); // Start text input when Escape is pressed
-                        CurrentState = TEXTINPUT; // Switch to text input state
-                        std::cout << CurrentState << "\n"; // Print the current state to console
-
-                    }
-                }
-                //chat packets 
-               
-               
-                else if (inputEvent.key.keysym.sym == SDLK_BACKSPACE && CurrentState == serverTEXTINPUT) {
-                    // Handle backspace to remove the last character
-                    if (!(serverTextInput.empty()))serverTextInput.pop_back();
-
-                }
-				else if (CurrentState == serverTEXTINPUT && (inputEvent.key.keysym.sym == SDLK_ESCAPE || inputEvent.key.keysym.sym==SDLK_RETURN)) {
-					// Stop server text input when Escape is pressed
-					CurrentState = MOVEMENT; // Switch back to movement state
-					SDL_StopTextInput(); // Stop text input
-					std::cout << "server text input stopped\n";
-                    std::cout << "server text input: " << serverTextInput << "\n"; // Print the server text input to console
-                    
-				}
-                else if (inputEvent.key.keysym.sym == SDLK_t && !(CurrentState==TEXTINPUT)) {
-                    if (CurrentState == TEXTINPUT || CurrentState == MOVEMENT) {
-                        SDL_StartTextInput(); // Start text input when 't' is pressed
-                        CurrentState = serverTEXTINPUT; // Switch to server text input state
-						serverTextInput = ""; // Clear previous server text input
-                        std::cout << "server text input\n";
-                    }
-
-                }
-				else if (inputEvent.key.keysym.sym == SDLK_ESCAPE) {
-					// Stop text input when Escape is pressed
-					SDL_StopTextInput();
-					CurrentState = MOVEMENT; // Switch to movement state
-					std::cout << "text input stopped\n";
-				}
-                else if (inputEvent.key.keysym.sym == SDLK_BACKSPACE) {
-                    // Handle backspace to remove the last character
-                    if (!(textInput.empty()))textInput.pop_back();
-
-                }
-            }
-            else if (inputEvent.type == SDL_TEXTINPUT && CurrentState == serverTEXTINPUT) {
-                // Handle text input here if needed
-                serverTextInput += inputEvent.text.text; // Append the new text input
-
-            }
-            else if (inputEvent.type == SDL_TEXTINPUT && CurrentState == TEXTINPUT) {
-                // Handle text input here if needed
-                textInput += inputEvent.text.text; // Append the new text input
-            }
-        }
-        if (CurrentState == MOVEMENT) {
-            const Uint8* keystate = SDL_GetKeyboardState(NULL);
-            if (x <= 0) x = 0;
-            if (x >= 600) x = 600;  // 800 - rect width
-            if (y <= 0) y = 0;
-            if (y >= 400) y = 400;  // 600 - rect height
-
-            if (keystate[SDL_SCANCODE_W]) {
-                y -= 5;
-            }
-            if (keystate[SDL_SCANCODE_S]) {
-                y += 5;
-            }
-            if (keystate[SDL_SCANCODE_A]) {
-                x -= 5;
-            }
-            if (keystate[SDL_SCANCODE_D]) {
-                x += 5;
-            }
-        }
+		// Handle events
+        eventHandler(inputEvent, running, &x, &y, &clicks,&CurrentState,&textInput,&serverTextInput);
+		enetEventHandler(&enetEvent, Host); // Handle ENet events
 
 
-
-
-       // game loop:
-
-
-
-
-
+        //rendering
         SDL_SetRenderDrawColor(renderer, 0, 0, 100, 255); // Blue
 
         SDL_Rect boxrect = { x, y, 200, 200 }; // Example rectangle
@@ -376,4 +376,3 @@ int main(int argc, char* argv[]) {
 
         return 0;
     }
-
